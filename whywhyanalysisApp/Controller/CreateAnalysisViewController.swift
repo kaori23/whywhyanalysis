4bf8bea90ca2a0b2ed82feb03dcc53fbf55fd9ae
kaori23 <liakanakikmura@gmail.com>
2020-11-22 16:38:30 +0900

    Fiex CreateAnalysisViewController
    fixed VariableName

--- whywhyanalysisApp/Controller/CreateAnalysisViewController.swift
+++ whywhyanalysisApp/Controller/CreateAnalysisViewController.swift
@@ -14,16 +14,16 @@ class CreateAnalysisViewController: UIViewController {
     
     @IBOutlet weak var problemTextField: UITextField!
     
-    @IBOutlet weak var oneWayTextField: UITextField!
+    @IBOutlet weak var oneWhyTextField: UITextField!
     
-    @IBOutlet weak var twoWayTextField: UITextField!
+    @IBOutlet weak var twoWhyTextField: UITextField!
     
-    @IBOutlet weak var threeTextField: UITextField!
+    @IBOutlet weak var threeWhyTextField: UITextField!
     
-    @IBOutlet weak var fourTextFIeld: UITextField!
+    @IBOutlet weak var fourWhyTextFIeld: UITextField!
     
     
-    @IBOutlet weak var fiveTextField: UITextField!
+    @IBOutlet weak var fiveWhyTextField: UITextField!
     
     
     @IBOutlet weak var measuresTextField: UITextField!
@@ -35,20 +35,34 @@ class CreateAnalysisViewController: UIViewController {
     
     
     @IBAction func confirmPopOverClick(_ sender: Any) {
-        if(ValidateUtility.textEmplyCheck(optinalText: problemTextField.text) == true)
-            && (ValidateUtility.textEmplyCheck(optinalText: oneWayTextField.text) == true)
-            && (ValidateUtility.textEmplyCheck(optinalText: measuresTextField.text) == true) {
+        if(ValidateUtility.textEmplyCheck(optinalText: problemTextField.text))
+            && (ValidateUtility.textEmplyCheck(optinalText: oneWhyTextField.text))
+            && (ValidateUtility.textEmplyCheck(optinalText: measuresTextField.text)) {
             let storyboard: UIStoryboard = self.storyboard!
             let nextViewController = storyboard.instantiateViewController(withIdentifier: "resistAnalysis") as! RegistAnalysisViewController
+            // 2WHY以降は任意入力の為何も入力されていない場合は""を代入する
+            var twoWhy = ""
+            var threeWhy = ""
+            var fourWhy = ""
+            var fiveWhy = ""
+
+            if(ValidateUtility.textEmplyCheck(optinalText:twoWhyTextField.text)) {
+                twoWhy = twoWhyTextField.text!
+            }
+            if(ValidateUtility.textEmplyCheck(optinalText: threeWhyTextField.text)) {
+                threeWhy = threeWhyTextField.text!
+            }
             
-            nextViewController.measures = measuresTextField.text!
-            nextViewController.problem = problemTextField.text!
-            nextViewController.oneWhy = oneWayTextField.text!
-            nextViewController.twoWhy = twoWayTextField.text!
-            nextViewController.threeWhy = threeTextField.text!
-            nextViewController.fourWhy = fourTextFIeld.text!
-            nextViewController.fiveWhy = fiveTextField.text!
+            if(ValidateUtility.textEmplyCheck(optinalText: fourWhyTextFIeld.text)) {
+                fourWhy = fourWhyTextFIeld.text!
+            }
+            if(ValidateUtility.textEmplyCheck(optinalText: fiveWhyTextField.text)) {
+                fiveWhy = fiveWhyTextField.text!
+            }
             
+            let whywhyAnalysis = WhywhyAnalysis(problem: problemTextField.text!, measures: measuresTextField.text!, oneWhy: oneWhyTextField.text!, twoWhy: twoWhy, threeWhy: threeWhy, fourWhy: fourWhy, fiveWhy: fiveWhy)
+
+            nextViewController.whywhyAnalysis = whywhyAnalysis
             navigationController?.pushViewController(nextViewController, animated: true)
         } else {
             print("空の項目あり")
