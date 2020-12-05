7a535ee0768289195bff0c4af92c46c3eefa9f78
kaori23 <liakanakikmura@gmail.com>
2020-12-05 16:42:47 +0900

    R.swift導入

---
+++ whywhyanalysisApp/WhyWhyAnalysisView.swift
@@ -0,0 +1,30 @@
+//
+//  WhyWhyAnalysisView.swift
+//  whywhyanalysisApp
+//
+//  Created by 長塚かおり on 2020/12/02.
+//  Copyright © 2020 長塚かおり. All rights reserved.
+//
+
+import UIKit
+
+class WhyWhyAnalysisView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
