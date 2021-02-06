0ef184ac85bc4a862430349ef9a35e41dcc815cb
kaori23 <liakanakikmura@gmail.com>
2021-02-06 18:30:14 +0900

    ステータス変更処理修正のため分析カスタムセルを修正

---
+++ whywhyanalysisApp/Src/Controller/ChangeStatusViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChangeStatusViewController.swift
+//  whywhyanalysisApp
+//
+//  Created by 長塚かおり on 2021/02/02.
+//  Copyright © 2021 長塚かおり. All rights reserved.
+//
+
+import UIKit
+
+class ChangeStatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- whywhyanalysisApp/Src/View/ShowAnalysisListCustumCell.swift
+++ whywhyanalysisApp/Src/View/ShowAnalysisListCustumCell.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 internal class ShowAnalysisListCustumCell: UITableViewCell {
+    internal let status = ""
     @IBOutlet internal weak var measuresLabel: UILabel!
     @IBOutlet internal weak var problemLabel: UILabel!
-    @IBOutlet internal weak var statusLabel: UILabel!
+    @IBOutlet internal weak var statusButton: UIButton!
+
+    override internal func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
 
     override internal func awakeFromNib() {
         super.awakeFromNib()
         // レイアウト設定
         let backgroundColor = UIColor(red: 113 / 255, green: 205 / 255, blue: 255 / 255, alpha: 1)
-        statusLabel.backgroundColor = backgroundColor
-        statusLabel.textColor = .white
-        statusLabel.textAlignment = NSTextAlignment.center
-    }
-
-    override internal func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        statusButton.backgroundColor = backgroundColor
+        statusButton.setTitleColor(.white, for: .normal)
     }
 }
